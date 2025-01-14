03be5cefe225f9518fd7993f791f685c279dddbf
wixoaGit <wixoag@gmail.com>
2025-01-14 10:01:29 -0500

    Desperately trying not to setup Docker locally

--- docker/run.sh
+++ docker/run.sh
@@ -3,8 +3,8 @@ cp /app/code/* .
 echo "---Preamble---"
 echo Included compiler arguments: "$@"
 echo "---Start Compiler---"
-DMCompiler "$@" test.dme
+/usr/bin/DMCompiler "$@" test.dme
 echo "---End Compiler---"
 echo "---Start Server---"
-Robust.Server --config-file server_config.toml --cvar opendream.json_path=/app/test.json
+/usr/bin/Robust.Server --config-file server_config.toml --cvar opendream.json_path=/app/test.json
 echo "---End Server---"
